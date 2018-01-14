72d9fbfc8a2bd0c047b8efc1e82ec9dd594b5ae0
BoD <BoD@JRAF.org>
2018-01-14 19:28:11 +0100

    Clock

--- minitel-cli/src/main/kotlin/org/jraf/minitel/cli/Main.kt
+++ minitel-cli/src/main/kotlin/org/jraf/minitel/cli/Main.kt
@@ -2,12 +2,19 @@ package org.jraf.minitel.cli
 
 import com.beust.jcommander.JCommander
 import org.jraf.minitel.lib.util.escaping.CLEAR_SCREEN_AND_HOME
+import org.jraf.minitel.lib.util.escaping.COLOR_FOREGROUND_2
+import org.jraf.minitel.lib.util.escaping.COLOR_FOREGROUND_4
+import org.jraf.minitel.lib.util.escaping.COLOR_FOREGROUND_7
 import org.jraf.minitel.lib.util.escaping.SCREEN_HEIGHT
 import org.jraf.minitel.lib.util.escaping.SCREEN_WIDTH
+import org.jraf.minitel.lib.util.escaping.SIZE_DOUBLE
+import org.jraf.minitel.lib.util.escaping.SIZE_WIDE
 import org.jraf.minitel.lib.util.escaping.moveCursor
 import java.io.BufferedWriter
 import java.io.FileOutputStream
 import java.io.OutputStreamWriter
+import java.text.SimpleDateFormat
+import java.util.Date
 
 
 class Main {
@@ -27,20 +34,21 @@ class Main {
 
             BufferedWriter(OutputStreamWriter(FileOutputStream("/mnt/n/tmp/tmp"))).use {
                 it.write(CLEAR_SCREEN_AND_HOME)
-                it.write(moveCursor(0, 0))
-                it.write("0,0")
-                it.write(moveCursor(1, 1))
-                it.write("1,1")
-                it.write(moveCursor(2, 2))
-                it.write("2,2")
-                it.write(moveCursor(0, SCREEN_WIDTH - 1))
-                it.write("7")
-                it.write(moveCursor(SCREEN_HEIGHT - 1, 0))
-                it.write("L")
-                it.write(moveCursor(SCREEN_HEIGHT - 1, SCREEN_WIDTH - 1))
-                it.write("J")
+                while (true) {
+                    val date = Date()
+                    val timePlain = timePlain(date)
+                    val timeFormatted = timeFormatted(date)
+                    it.write(moveCursor((SCREEN_WIDTH - timePlain.length * 2) / 2, SCREEN_HEIGHT / 2))
+                    it.write(SIZE_DOUBLE)
+                    it.write(timeFormatted)
+                    it.flush()
 
+                    Thread.sleep(1000)
+                }
             }
         }
+
+        fun timePlain(date: Date) = SimpleDateFormat("HH:mm:ss").format(date)
+        fun timeFormatted(date: Date) = SimpleDateFormat("HH'$COLOR_FOREGROUND_2':'$COLOR_FOREGROUND_7'mm'$COLOR_FOREGROUND_2':'$COLOR_FOREGROUND_4$SIZE_WIDE'ss").format(date)
     }
 }
