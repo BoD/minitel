26068935f22f40f472741de18feecd0eb028ac83
BoD <BoD@JRAF.org>
2018-01-14 17:47:30 +0100

    Initial commit

---
+++ minitel-cli/src/main/kotlin/org/jraf/logz/cli/Arguments.kt
@@ -0,0 +1,14 @@
+package org.jraf.logz.cli
+
+import com.beust.jcommander.Parameter
+
+
+class Arguments {
+    @Parameter(
+            names = arrayOf("-h", "--help"),
+            description = "Show this help",
+            help = true
+    )
+    var help: Boolean = false
+
+}
